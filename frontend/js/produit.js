0a5d89555ed8f52140b1d18e5d13bf78a2fdfe8f
Virgile Guglielmi <virgile.gug@gmail.com>
2021-03-18 02:04:06 +0100

    :recycle: product refacto
    product refacto

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -1,60 +1,47 @@
 /* GET url id */
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get("id");
-
-/* GET url id short */
-console.log(new URL(window.location.href).searchParams.get("id"));
-
+const id = new URL(window.location.href).searchParams.get("id");
 /* Vars */
-const myRequest = new Request(`${apiUrl}/api/cameras/` + id);
 let cart = [];
 
-/* HARD QuerySelector */
-const vcamName = document.querySelector(".card-title");
-const vcamImage = document.querySelector(".card img");
-const vcamPrice = document.querySelector(".price");
-const vcamDescription = document.querySelector(".card-text");
-const vcamDropdown = document.querySelector(".dropdown-menu");
-const vcamAdd = document.querySelector(".addToCart");
 
-/* API Fetch */
-fetch(myRequest)
-  .then(res => {
-    if (res.ok) {
-      res.json().then(data => {
-        console.log(data);
-        vcamName.textContent = (data.name);
-        vcamImage.setAttribute("src", data.imageUrl);
-        vcamImage.setAttribute("alt", data.name);
-        vcamPrice.textContent = (data.price / 100 + "€");
-        vcamDescription.textContent = (data.description);
-        /* Dropdown menu feeding */
-        for (let i in data.lenses) {
-          let newDropdownItem = document.createElement("a");
-          newDropdownItem.classList.add("dropdown-item");
-          newDropdownItem.textContent = (data.lenses[i])
-          vcamDropdown.appendChild(newDropdownItem);
-        }
-        /* Cart feeding */
-        console.log(vcamAdd);
-        vcamAdd.addEventListener('click', event => {
-          let cart = [];
-          cart.push(data._id);
-          localStorage.setItem("productId", cart);
-          console.log(cart);
-        })
-        
+(async () => {
+  const product = await getData();
+  console.log("Request result :", product);
+  fillMarkup(product);
+})();
 
-      });
-    }
-    else {
-      throw new Error();
-    }
-  })
-  .catch(err => {
-    console.log("Fetch Error :", err);
-  });
+function getData() {
+  return fetch(`${apiUrl}/api/cameras/` + id)
+    .then(res => {
+      console.log("HTTP Request Status :", res.status);
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((product) => product)
+    .catch(err => {
+      console.log(err);
+      alert("La connexion au serveur à échoué, veuillez réactualiser la page !");
+    });
+};
 
-/*formatPrice*/
-/*-----------------------------------------------------------*/
+function fillMarkup(product) {
+  document.querySelector(".card-title").textContent = product.name;
+  document.querySelector(".card img").src = product.imageUrl;
+  document.querySelector(".card-title").alt = product.name;
+  document.querySelector(".price").textContent = (product.price / 100 + "€");
+  document.querySelector(".card-text").textContent = (product.description);
+  /* Dropdown menu feeding */
+  for (let i in product.lenses) {
+    let newDropdownItem = document.createElement("a");
+    newDropdownItem.classList.add("dropdown-item");
+    newDropdownItem.textContent = (product.lenses[i])
+    document.querySelector(".dropdown-menu").appendChild(newDropdownItem);
+  }
+}
+
+
+
+
+
+
