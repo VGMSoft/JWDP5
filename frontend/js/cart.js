b56ce9797ced7de321fd25cee8cae3b45722ab9c
Virgile Guglielmi <virgile.gug@gmail.com>
2021-04-03 19:26:43 +0200

    :ambulance: cart OK
    cart OK

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,9 +1,5 @@
 class Cart {
 
-
-    // localStorage.setItem('cart', JSON.stringify(products))
-
-
     getOption(onOptionSelected) {
         const lenseSelector = document.querySelector(".lenseSelector")
         lenseSelector.onchange = () => {
@@ -12,26 +8,20 @@ class Cart {
         }
     }
 
-    createProductObject(product){
-        //new Product Object
-        let productObject = {}
-        //Adding attribute to cartObject
-        productObject.name = product.name
-        productObject.price = (product.price / 100 + "€")
-        productObject.option = "option"
-        productObject.quantity = 1
-        cart[id] = productObject
-    }
-
-
-
     addItem(product) {
         //Create cart
         const cart = JSON.parse(localStorage.getItem('cart')) || {}
         const id = (product._id)
         //test if product exist in cart
         if (cart[id] == null) {
-            createProductObject(product)
+            //new Product Object
+            let productObject = {}
+            //Adding attribute to cartObject
+            productObject.name = product.name
+            productObject.price = (product.price / 100 + "€")
+            productObject.option = "option"
+            productObject.quantity = 1
+            cart[id] = productObject
         } else {
             //increasing quantity
             cart[id].quantity++
