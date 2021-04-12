8d0d4b5e37cee10bcbc29033bda3f5749fd74189
Virgile Guglielmi <virgile.gug@gmail.com>
2021-04-12 12:38:35 +0200

    :art: Improve structure
    Improve structure

--- frontend/js/pages/confirmation.js
+++ frontend/js/pages/confirmation.js
@@ -1,8 +1,8 @@
-//Cleaning Cart
-cart.emptyCart()
-
 document.querySelector(".orderId").innerHTML = getParam("orderId")
 document.querySelector(".total").innerHTML = `${getParam("total")}&#128;`
+
+//Cleaning Cart
+cart.emptyCart()
 
 function getParam(param) {
   return new URL(window.location.href).searchParams.get(param)
