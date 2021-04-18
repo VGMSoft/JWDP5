56f5ca13bd6db5071b9f3ea77dcb7db3f8050864
Virgile Guglielmi <virgile.gug@gmail.com>
2021-04-18 02:19:08 +0200

    :white_check_mark: tests written
    tests written

--- frontend/js/pages/confirmation.js
+++ frontend/js/pages/confirmation.js
@@ -1,10 +1,17 @@
-//Display data
-document.querySelector(".orderId").innerHTML = getParam("orderId")
-document.querySelector(".total").innerHTML = `${getParam("total")}&#128;`
+(() => {
+  displayData()
+  cleanCart()
+})()
 
-//Cleaning Cart
-localStorage.removeItem("cart")
-localStorage.removeItem("amount")
+function displayData() {
+  document.querySelector(".orderId").innerHTML = getParam("orderId")
+  document.querySelector(".total").innerHTML = `${getParam("total")}&#128;`
+}
+
+function cleanCart() {
+  localStorage.removeItem("cart")
+  localStorage.removeItem("amount")
+}
 
 function getParam(param) {
   return new URL(window.location.href).searchParams.get(param)
