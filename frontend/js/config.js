ef588974161b0d1a4c9348c9fa21daba5c54ddc9
Virgile Guglielmi <virgile.gug@gmail.com>
2021-04-18 15:59:11 +0200

    :art: minified JS on prod mode
    minified JS on prod mode

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,1 @@
-let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ?
-  "http://localhost:3000" :
-  "https://oc-orinoco-api.herokuapp.com"
+let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:3000" : "https://oc-orinoco-api.herokuapp.com"
