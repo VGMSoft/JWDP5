047469b16ce189165035cf106b2993da1c42284e
Virgile Guglielmi <virgile.gug@gmail.com>
2021-04-18 02:02:25 +0200

    :fire: delete console.log for production
    delete console.log for production

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,3 @@
 let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ?
   "http://localhost:3000" :
   "https://oc-orinoco-api.herokuapp.com"
-console.log(`API Path : ${apiUrl}`)
